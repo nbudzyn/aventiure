b49a7b9bdb5caba4abdafd29e27754d26d4571b3
nbudzyn <nbudzyn@gmx.de>
2021-10-12 18:08:50 +0200

    #2 Upgrade android-tools, Java 11, Schnneeweißchen und Rosenrot

---
+++ app/src/main/java/de/nb/aventiure2/data/world/syscomp/story/impl/SchneeweisschenUndRosenrotStoryNode.java
@@ -0,0 +1,27 @@
+package de.nb.aventiure2.data.world.syscomp.story.impl;
+
+public class SchneeweisschenUndRosenrotStoryNode {
+    // FIXME Schneeweißchen und Rosenrot!
+    //
+    // FIXME Zwerg hatcBarr einheklemmt, SC zieht heraus Bart beinahe was angerissem
+    //
+    // FIXME Zwerg schimpft...
+    //
+    // FIXME Bär erzählt Verwandlungsgeschichte
+    //
+    // FIXME Schere
+    //
+    // FIXME Zwerg verheddert sich beim Angeln
+    //
+    // FIXME Sc schneidet bRt ab
+    //
+    // FIXME Zwerg schimpft: schlimmer als die adler, die einen auf der heide packen
+    //
+    // FIXME Sc trifft bären, bart anscheinend nicht kurz grnug
+    //
+    // FIXME Sc erlebt auf der heide
+    //  Zufällig mit wie adler zwerg greift
+    //  Sc hält zwerg fest, bart reißt ab
+    //
+    // FIXME Bär... zurückverwandelt
+}
